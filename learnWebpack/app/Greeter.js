b9f9808e366e3720e09289f88dac6d58f17849c2
812913 <15928543307@163.com>
2018-04-19 14:49:36 +0800

    test again

--- learnWebpack/app/Greeter.js
+++ learnWebpack/app/Greeter.js
@@ -3,3 +3,4 @@ module.exports=function(){
     greet.textContent="Hi three and greetings!";
     return greet;
 };
+
--- learnWebpack/app/main.js
+++ learnWebpack/app/main.js
@@ -1,2 +1,3 @@
 const greeter = require('./Greeter.js');
 document.querySelector("#root").appendChild(greeter());
+
