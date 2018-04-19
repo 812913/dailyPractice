dbd030436ca764d8a8cc85383d65693756112686
812913 <15928543307@163.com>
2018-04-19 20:52:50 +0800

    third commit

--- learnWebpack/webpack.config.js
+++ learnWebpack/webpack.config.js
@@ -1,7 +1,47 @@
-module.export={
-    entry:__dirname+"/app/main.js",//唯一入口文件，__dirname是node.js中的一个全局变量，它指向当前执行脚本所在目录
+//const path=require('path');
+module.exports={
+    devtool:'eval-source-map',//生成source maps，使调试更容易
+    entry:'./src/main.js',//唯一入口文件，__dirname是node.js中的一个全局变量，它指向当前执行脚本所在目录
     output:{
-        path:__dirname+"/public",//打包后的文件存放的地方
-        filename:"bundle.js"//打包后输出的文件的文件名
+        filename:"bundle.js",//打包后输出的文件的文件名
+        path:__dirname+'/dist'//打包后的文件存放的地方
+    },
+    devServer:{
+        contentBase:"./dist",
+        port:8080,//默认监听端口
+        inline:true,//源文件改变时，自动刷新页面
+        historyApiFallback:true//开发单页面时非常有用。如果设置为true，所有的跳转将指向index.html
+    },
+    module:{
+        rules:[
+            {
+                test:/(\.jsx|\.js)$/,//必须，一个用以匹配loaders所处理文件的拓展名的正则表达式
+                use:{
+                    loader:"babel-loader",//loader的名称
+                    options:{
+                        presets:[
+                            "env","react"
+                        ]
+                    }
+                },
+                exclude:/node_modules///屏蔽不需要处理的文件
+            },{
+                test:/\.css$/,
+                use:[
+                    {
+                        loader:"style-loader"
+                    },
+                    {
+                        loader:"css-loader",
+                        options:{
+                            modules:true,//是否启用css modules
+                            localIdentName:'[name]__[local]--[hash:base64:5]'//指定css的类名格式
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
+//console.log(__dirname);//c:\Users\15928\Desktop\Practice\dailyPractice\learnWebpack
+//console.log(module.exports.output.path);//c:\Users\15928\Desktop\Practice\dailyPractice\learnWebpack/dist
